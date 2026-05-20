21161083255061bf0719e9e0befca1d78daa5dcb
kevin <xiodreick@gmail>
2026-05-20 09:43:42 -0400

    ultima prueba

--- GestorDeBiblioteca.py
+++ GestorDeBiblioteca.py
@@ -1,14 +1,14 @@
 au="usuario"
 bc="contraseña"
 contador=4
-while contador<=3:
-    a=input("ingrese usuario: ")
-    b=input("ingrese contraseña: ")
+while contador>0:
+    a=input("ingrese usuario: ").strip()
+    b=input("ingrese contraseña: ").strip()
     a=a.lower()
     b=b.lower()
     if  a==au and b==bc:
         print("acceso concedido")
-        contador=contador+1
+        contador=contador-1
     else:
         print("usuario o contraseñas incorrectos")
 
